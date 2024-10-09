ce5fc5d8b1b1d1e7dcb61d464b9d21e865286cd2
gholamreza falahzade <gholamreza.fallah2208@gmail.com>
2024-10-09 13:08:19 +0330

    test stage 2

--- components/ExampleComponent.jsx
+++ components/ExampleComponent.jsx
@@ -1,9 +1,7 @@
-const ExampleComponent = () => {
+function ExampleComponent() {
     return (
         <div style={{ padding: "20px", backgroundColor: "#f0f0f0" }}>
             Hello from Example Component!
         </div>
     );
-};
-
-export default ExampleComponent;
+}
