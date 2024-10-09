4d9019674672a5498766f1874b9123c514cc6e98
gholamreza falahzade <gholamreza.fallah2208@gmail.com>
2024-10-09 12:48:11 +0330

    first test

---
+++ components/ExampleComponent.jsx
@@ -0,0 +1,9 @@
+const ExampleComponent = () => {
+    return (
+        <div style={{ padding: "20px", backgroundColor: "#f0f0f0" }}>
+            Hello from Example Component!
+        </div>
+    );
+};
+
+export default ExampleComponent;
---
+++ public/components/ExampleComponent.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var ExampleComponent = function ExampleComponent() {
+  return /*#__PURE__*/React.createElement("div", {
+    style: {
+      padding: "20px",
+      backgroundColor: "#f0f0f0"
+    }
+  }, "Hello from Example Component!");
+};
+var _default = exports["default"] = ExampleComponent;
